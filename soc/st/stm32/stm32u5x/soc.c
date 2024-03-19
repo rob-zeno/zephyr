51a497e88915a4cda7fff7babd983cee8ba027f6
Rob Newberry <rob@zenomoto.com>
2024-03-18 18:49:59 -0700

    Add support for the STM32U545xx SoC, as well as the Nucleo U545RE-Q board.
    NOTE: These changes are dependent on changes to the stm32u5 HAL module,
    documented in https://github.com/zephyrproject-rtos/hal_stm32/pull/198.
    
    Signed-off-by: Rob Newberry <rob@zenomoto.com>

--- soc/st/stm32/stm32u5x/soc.c
+++ soc/st/stm32/stm32u5x/soc.c
@@ -44,8 +44,10 @@ static int stm32u5_init(void)
 
 	if (IS_ENABLED(CONFIG_DT_HAS_ST_STM32_UCPD_ENABLED) ||
 		!IS_ENABLED(CONFIG_USB_DEVICE_DRIVER)) {
+#ifdef UCPD1
 		/* Disable USB Type-C dead battery pull-down behavior */
 		LL_PWR_DisableUCPDDeadBattery();
+#endif
 	}
 
 	/* Power Configuration */
