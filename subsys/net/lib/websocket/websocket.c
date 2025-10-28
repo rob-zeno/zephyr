0f90e937be8588fd82dc7fc324c3d212b32d64ba
Rob Newberry <rob@zenomoto.com>
2025-10-28 12:49:56 -0700

    update to 4.2 HTTP client API

--- subsys/net/lib/websocket/websocket.c
+++ subsys/net/lib/websocket/websocket.c
@@ -151,7 +151,7 @@ static struct websocket_context *websocket_find(int real_sock)
 	return ctx;
 }
 
-static void response_cb(struct http_response *rsp,
+static int response_cb(struct http_response *rsp,
 			enum http_final_call final_data,
 			void *user_data)
 {
@@ -166,6 +166,8 @@ static void response_cb(struct http_response *rsp,
 			rsp->data_len);
 		ctx->all_received = true;
 	}
+
+	return 0;
 }
 
 static int on_header_field(struct http_parser *parser, const char *at,
